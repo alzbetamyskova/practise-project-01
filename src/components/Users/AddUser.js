2163de15d25212408c7172e7bc43441720111607
Alzbeta Myskova <betka.myskova@seznam.cz>
2021-08-31 11:22:16 +0200

    přidána componenta Card

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from '../UI/Card';
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -6,15 +7,19 @@ const AddUser = (props) => {
   };
 
   return (
-  <form onSubmit={addUserHandler}>
+  <Card>
+    <form onSubmit={addUserHandler}>
 
-    <label htmlFor='username'>Uživatelské jméno</label>
-    <input id='username' type='text' />
-    <label htmlFor='age'>Věk (roky)</label>
-    <input id='age' type='number' />
-    <button type='submit'>Přidat uživatele</button>
+      <label htmlFor='username'>Uživatelské jméno</label>
+      <input id='username' type='text' />
 
-  </form>
+      <label htmlFor='age'>Věk (roky)</label>
+      <input id='age' type='number' />
+      
+      <button type='submit'>Přidat uživatele</button>
+
+    </form>
+  </Card>
   );
 };
 
